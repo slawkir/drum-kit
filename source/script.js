59985635eb0ec5f532f829f867fd0e1a7ad2d62d
slawkir <slawkir1984@gmail.com>
2021-08-24 01:47:10 +0400

    feat: improoving of playing of sound

--- source/script.js
+++ source/script.js
@@ -1,8 +1,30 @@
 const playSound = (e) => {
   const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+ audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
 }
 
+ function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+
+
 window.addEventListener("keydown", playSound);
+ 
+const playSoundClick = (event) => {
+  const audio = document.querySelector(`audio[data-key="${event.target.getAttribute("data-key")}"]`);
+  if (!audio) return;
+ audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+
+window.addEventListener("click", playSoundClick);
